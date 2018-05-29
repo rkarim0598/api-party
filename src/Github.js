4a0bbdf0aaa5f608c9d7f34bc7abaea78fe0c9a1
Rayyan Karim <rkarim0598@gmail.com>
2018-05-29 15:25:34 -0400

    Fix pushing onto url

--- src/Github.js
+++ src/Github.js
@@ -3,39 +3,42 @@ import React, { Component } from 'react'
 import './Github.css'
 
 class Github extends Component {
-    state = {
-        username: '',
-    }
+  state = {
+    username: '',
+  }
 
-    handleChange = (ev) => {
-        this.setState({ username: ev.target.value })
-    }
+  handleChange = (ev) => {
+    this.setState({ username: ev.target.value })
+  }
 
-    handleSubmit = (ev) => {
-        ev.preventDefault()
-        this.props.history.push(`/github/${this.state.username}`)
-    }
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.props.history.push(`/github/${this.state.username}`)
+  }
 
-    render() {
-        return (
-            <div className="Github">
-                <img
-                    src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
-                    alt="GitHub"
-                    className="logo"
-                />
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <input type="text" 
-                                onClick={this.handleChange} />
-                    </div>
-                    <div>
-                        <button type="submit">Look up GitHub user</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Github">
+        <img
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="GitHub"
+          className="logo"
+        />
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Look up GitHub user</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Github
