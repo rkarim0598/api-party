38054ca49f7543801a12aa8d0ed4d7e3d817207f
Rayyan Karim <rkarim0598@gmail.com>
2018-05-29 15:53:58 -0400

    Pass props explicitly to fetchUserData

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import './GithubUser.css'
+
 class GithubUser extends Component {
     constructor(props) {
         super(props)
@@ -10,15 +12,28 @@ class GithubUser extends Component {
 
         this.fetchUserData()
     }
-    fetchUserData = () => {
+
+    componentWillReceiveProps = (newProps) => {
+        const locationChanged = newProps.location !== this.props.location
+        if (locationChanged) {
+            this.fetchUserData(newProps)
+        }
+    }
+    fetchUserData = (props) => {
         fetch(`https://api.github.com/users/${this.props.match.params.username}`)
             .then(response => response.json())
             .then(user => this.setState({ user }))
     }
     render() {
+        const { user } = this.state
         return (
             <div className="GithubUser">
-                <h1>GitHub user: {this.props.match.params.username}</h1>
+                <img src={user.avatar_url} />
+                <h2>{user.login}</h2>
+                <h3>followers: {user.followers}</h3>
+                <h3>following: {user.following}</h3>
+                <h3>location: {user.location}</h3>
+                <a href={user.html_url} target="_blank">Profile</a>
             </div>
         )
     }
