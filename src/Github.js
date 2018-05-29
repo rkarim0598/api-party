75b036f3d3038a298847bc19b2789611bc4285c1
Rayyan Karim <rkarim0598@gmail.com>
2018-05-29 15:24:28 -0400

    Add submit handler that pushes username onto history

--- src/Github.js
+++ src/Github.js
@@ -10,6 +10,12 @@ class Github extends Component {
     handleChange = (ev) => {
         this.setState({ username: ev.target.value })
     }
+
+    handleSubmit = (ev) => {
+        ev.preventDefault()
+        this.props.history.push(`/github/${this.state.username}`)
+    }
+
     render() {
         return (
             <div className="Github">
@@ -18,7 +24,7 @@ class Github extends Component {
                     alt="GitHub"
                     className="logo"
                 />
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <input type="text" 
                                 onClick={this.handleChange} />
