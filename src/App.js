1e8e289ba768977643d1425d4d94300e729920df
Rayyan Karim <rkarim0598@gmail.com>
2018-05-29 15:18:47 -0400

    Tie value of input to state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Route, Switch, NavLink } from 'react-router-dom'
 
-import './App.css';
+import './App.css'
+import Github from './Github'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h3>Hello</h3>
+          <h3>Ain't no party like an</h3>
           <h1>API Party</h1>
         </div>
-        <ul>
+        <ul className="navLinks">
           <li>
             <NavLink to="/github">GitHub API</NavLink>
           </li>
         </ul>
         <Switch>
-          <Route path="/github" render={() => <h1>Github</h1>} />
-          <Route render={() => <p>To get started, click one of the links above</p>} />
+          <Route path="/github" component={Github} />
+          <Route render={() => <p>To get started, click one of the links above.</p>} />
         </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
